3ef10503427502c127ef2ea9090ccb1685b1a23f
Jasmine <jasmine.kim.18@gmail.com>
2020-01-12 21:05:08 -0800

    Script to condense multiple dataframes

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from pandas.io.json import json_normalize
 import pickle
 import numpy as np
 from time import sleep
+import os
 
-def datamine():
+def get_breed_count(r_filename, w_filename):
     # Pull data from .pkl files
-    df = DogFinder()
-    unpickled_df = pd.read_pickle("50miles.pkl")
+    unpickled_df = pd.read_pickle(r_filename + ".pkl")
     unpickled_breeds = pd.read_pickle("breeds.pkl")
     #print unpickled_df
     #print unpickled_breeds
@@ -33,9 +33,9 @@ def datamine():
         if col == 'contact.address.postcode':
             continue # for now
 
-    unpickled_breeds.to_pickle('breedcount_50miles.pkl')
+    unpickled_breeds.to_pickle(w_filename + '.pkl')
     unpickled_breeds = unpickled_breeds.sort_values(by=['count'], ascending=False)
-    unpickled_breeds.to_csv(r'breedcount.csv', index=False, encoding='utf-8')
+    unpickled_breeds.to_csv(w_filename + '.csv', index=False, encoding='utf-8')
 
 
 def get_data_zipcode(zp):
@@ -64,10 +64,31 @@ def get_data_specific_zipcode(zipcode):
     animals.to_pickle(str(zipcode) + '.pkl')
     animals.to_csv(str(zipcode)+ '.csv', index=False, encoding='utf-8')
 
+def append_dataframes(w_filename):
+    df = DataFrame()
+    for filename in os.listdir("./test_pkl"):
+        if filename.endswith(".pkl"):
+            print filename
+            read_df = pd.read_pickle("./test_pkl/" + filename)
+            df = df.append([read_df])
+    # Removing all duplicate rows
+    df = df.drop_duplicates(subset ="animal_id", keep = 'first')
+    #df.to_pickle("cleaned.pkl")
+    df.to_pickle(w_filename + ".pkl")
+    print df
+    return df
 
 
 #get_data_specific_zipcode(90015)
-get_data_zipcode(90040)
+#get_data_zipcode(90074)
+
+df = append_dataframes("90001_90083")
+get_breed_count("90001_90083", "90001_90083_breed_count")
+
+
+
+
+
 # Count number of primary breeds and add count to breeds
 
 # Find all dogs within 50 miles of zipcode 92620
