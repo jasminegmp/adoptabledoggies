0091275fe52b97151d5328994250a1e0ab666603
Jasmine <jasmine.kim.18@gmail.com>
2020-01-17 22:18:18 -0800

    Frontend tied to backend

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -2,18 +2,18 @@ import json
 import flask
 import pandas as pd
 
-app = flask.Flask(__name__,
-            static_url_path='',
-            static_folder='build')
+app = flask.Flask("__main__")
 
 @app.route("/")
 def index():
+    return flask.render_template("index.html", token="HELLO!!!")
+    '''
     df = pd.read_csv('90001_90083_test/90001_90083_breed_count.csv')
     chart_data = df.to_dict(orient='records')
     chart_data = json.dumps(chart_data, indent=2)
     data = {'chart_data': chart_data}
     return flask.render_template("index.html", data=data, token="HELLO!!!")
+    '''
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+app.run(debug=True)
