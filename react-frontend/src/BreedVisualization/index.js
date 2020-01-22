c2b5269ec6976c36b0d833f4151b13d7ae289169
Jasmine <jasmine.kim.18@gmail.com>
2020-01-22 11:30:34 -0800

    Displaying breed results into D3

--- react-frontend/src/BreedVisualization/index.js
+++ react-frontend/src/BreedVisualization/index.js
@@ -8,9 +8,9 @@ class BreedVisualization extends React.Component {
         this.state = {
             loading: true,
             results: this.props.data.data, 
-            height: 700,
-            width: 1000,
-            margin: {top: 20, right: 20, bottom: 30, left: 40}
+            height: 500,
+            width: 500,
+            margin: {top: 5, right: 5, bottom: 5, left: 5}
         };
       }
 
@@ -18,101 +18,69 @@ class BreedVisualization extends React.Component {
         //this.drawChart();
         this.drawBarChart(this.state.results.slice(0,5));// just get the top 5 dog breeds,
     }
-   
-    drawChart = () => {
-      const {width, height, data} = this.state;
-      let margin = {top: 20, right: 20, bottom: 30, left: 40};
-      /*
-      const svg = d3.select("body")
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .style("margin-left", 100);
-                      */
-
-      // set the ranges
-      let x = d3.scaleBand()
-        .range([0, width])
-        .padding(0.1);
-      let y = d3.scaleLinear()
-        .range([height, 0]);
-      
-      // append the svg object to the body of the page
-      // append a 'group' element to 'svg'
-      // moves the 'group' element to the top left margin
-      var svg = d3.select("body").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", 
-              "translate(" + margin.left + "," + margin.top + ")");
-      
-      data.forEach(function(d) {
-        
-        d.breed_name = d[1];
-        d.breed_count += d[2];
-        
-        // Scale the range of the data in the domains
-        x.domain(data.map(function(d) { return d.breed_name; }));
-        y.domain([0, d3.max(data, function(d) { return d.breed_name; })]);
-
-        console.log(data);
-        // append the rectangles for the bar chart
-        svg.selectAll("bar")
-            .data(data)
-          .enter().append("rect")
-            .attr("fill", "steelblue")
-            .attr("x", function(d) { return x(d.breed_name); })
-            .attr("width", x.bandwidth())
-            .attr("y", function(d) { return y(d.breed_count); })
-            .attr("height", function(d) { return height - y(d.breed_count); });
-
-      });
-
-      // add the x Axis
-      svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-  
-      // add the y Axis
-      svg.append("g")
-            .call(d3.axisLeft(y));
-
-
-
-/*
-        svg.selectAll("rect")
-            .data(data[2])
-            .enter()
-            .append("rect")
-            .attr("x", (d, i) => i * 70)
-            .attr("y", (d, i) => h - 10 * d)
-            .attr("width", 65)
-            .attr("height", (d, i) => d * 10)
-            .attr("fill", "green")
-
-        svg.selectAll("text")
-            .data(data[1])
-            .enter()
-            .append("text")
-            .text((d) => d)
-            .attr("x", (d, i) => i * 70)
-            .attr("y", (d, i) => h - (10 * d) - 3)*/
-    }
 
 
     drawBarChart(data)  {
       const {margin, width, height} = this.state;
 
+      // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+      let radius = Math.min(width, height) / 2 * 0.8;
+
 
       const svgCanvas = d3.select(this.refs.canvas)
         .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .style("border", "1px solid black")
+        .attr("viewBox", [-width / 2, -height / 2, width, height])
+        .style("border", "1px solid black");
     
       console.log(data);
 
+      let pie = d3.pie()
+        .sort(null)
+        .value(d => d.count)
+
+      let arc = d3.arc()
+        .innerRadius(0)
+        .outerRadius(Math.min(width, height) / 2 - 1)
+
+      // set the color scale
+      let color = d3.scaleOrdinal()
+        .domain(data.map(d => d.name))
+        .range(d3.quantize(t => d3.interpolateSpectral(t * 0.8 + 0.1), data.length).reverse())
+
+      const arcs = pie(data);
+
+      const arcLabel =  d3.arc().innerRadius(radius).outerRadius(radius);
+
+      svgCanvas.append("g")
+          .attr("stroke", "white")
+        .selectAll("path")
+        .data(arcs)
+        .join("path")
+          .attr("fill", d => color(d.data.name))
+          .attr("d", arc)
+        .append("title")
+          .text(d => `${d.data.name}: ${d.data.count.toLocaleString()}`);
+  
+        
+      svgCanvas.append("g")
+          .attr("font-family", "sans-serif")
+          .attr("font-size", 12)
+          .attr("text-anchor", "middle")
+        .selectAll("text")
+        .data(arcs)
+        .join("text")
+          .attr("transform", d => `translate(${arcLabel.centroid(d)})`)
+          .call(text => text.append("tspan")
+              .attr("y", "-0.4em")
+              .attr("font-weight", "bold")
+              .text(d => d.data.name))
+          .call(text => text.filter(d => (d.endAngle - d.startAngle) > 0.25).append("tspan")
+              .attr("x", 0)
+              .attr("y", "0.7em")
+              .attr("fill-opacity", 0.7)
+              .text(d => d.data.count.toLocaleString()));
+
+      /*
       const x = d3.scaleBand()
         .domain(d3.range(data.length))
         .range([margin.left, width - margin.right])
@@ -129,7 +97,7 @@ class BreedVisualization extends React.Component {
             .attr("y", d => y(d.count))
             .attr("height", d => y(0) - y(d.count))
             .attr("width", 100);
-
+*/
      /* svgCanvas.selectAll("text")
         .data(data[1])
         .enter()
