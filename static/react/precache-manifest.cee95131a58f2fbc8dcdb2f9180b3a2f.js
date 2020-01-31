02ce326be6cea94be4e0753dd7190d590489d6da
Jasmine <jasmine.kim.18@gmail.com>
2020-01-31 15:58:22 -0800

    Adding static and template flask files

---
+++ static/react/precache-manifest.cee95131a58f2fbc8dcdb2f9180b3a2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a6933f833e7d752bc2a5156671d002",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "e1955fb82ce99feeb01a",
+    "url": "/static/react/css/main.2c93d0d4.chunk.css"
+  },
+  {
+    "revision": "40ae4dc7b59e83b8f8b7",
+    "url": "/static/react/js/2.429e660b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/react/js/2.429e660b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1955fb82ce99feeb01a",
+    "url": "/static/react/js/main.766a64dd.chunk.js"
+  },
+  {
+    "revision": "921f5f20ddb9415678d6",
+    "url": "/static/react/js/runtime-main.ec3c495a.js"
+  }
+]);
